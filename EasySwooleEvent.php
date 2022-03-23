5e0035ebdb07feb5d3b24d6c87dc06566b11b675
aimer <15267840622@163.com>
2022-03-23 23:38:49 +0800

    easyswoole

---
+++ EasySwooleEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace EasySwoole\EasySwoole;
+
+
+use EasySwoole\EasySwoole\AbstractInterface\Event;
+use EasySwoole\EasySwoole\Swoole\EventRegister;
+use EasySwoole\FileWatcher\FileWatcher;
+use EasySwoole\FileWatcher\WatchRule;
+
+class EasySwooleEvent implements Event
+{
+    public static function initialize()
+    {
+        date_default_timezone_set('Asia/Shanghai');
+    }
+
+    public static function mainServerCreate(EventRegister $register)
+    {
+        $watcher = new FileWatcher();
+        $rule = new WatchRule(EASYSWOOLE_ROOT . "/App"); // 设置监控规则和监控目录
+        $watcher->addRule($rule);
+        $watcher->setOnChange(function () {
+            Logger::getInstance()->info('file change ,reload!!!');
+            ServerManager::getInstance()->getSwooleServer()->reload();
+        });
+        $watcher->attachServer(ServerManager::getInstance()->getSwooleServer());
+    }
+}
